6a97709efcddb51a5b02ad0c225e397b677e0a87
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-04-28 20:24:05 +0300

    Update docs repo

--- src/views/NotFound.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="not-found">
-    <h2>Page non trouvée</h2>
-    <p>La page demandée n'existe pas.</p>
-    <router-link to="/">Retour à l'accueil</router-link>
-  </div>
-</template>
