5843124a3c9099017f47f9c1463edd1e34b05aef
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-06-03 11:28:40 +0300

    chore: add Google Ads tag to docs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1433,6 +1433,7 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag("js", new Date());
 gtag("config", "G-1B67VYZMXF");
+gtag("config", "AW-17078961408");
 `,
     ],
   ],
